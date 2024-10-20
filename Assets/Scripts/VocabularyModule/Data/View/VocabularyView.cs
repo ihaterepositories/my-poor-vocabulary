e070a9f186a86993d3c4056e24c321296d09bb6f
Bohutskiy Oleh <bogucukij@gmail.com>
2024-10-20 23:00:34 +0300

    search bar in vocabulary view

--- Assets/Scripts/VocabularyModule/Data/View/VocabularyView.cs
+++ Assets/Scripts/VocabularyModule/Data/View/VocabularyView.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 using VocabularyModule.Data.Models;
 using VocabularyModule.Data.View.Sorting;
 using VocabularyModule.Data.View.Sorting.UI;
@@ -12,9 +15,11 @@ namespace VocabularyModule.Data.View
     public class VocabularyView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI textField;
+        [SerializeField] private InputField searchInput;
         
         private VocabularyController _vocabularyController;
-        private List<Word> _words;
+        private List<Word> _vocabulary;
+        private int _lastSortType;
         
         [Inject]
         private void Construct(VocabularyController vocabularyController)
@@ -25,7 +30,12 @@ namespace VocabularyModule.Data.View
         private void Start()
         {
             LoadVocabulary();
-            ShowSorted(0);
+            
+            _lastSortType = 0;
+            searchInput.onValueChanged.AddListener(_ => ShowSearched());
+            
+            Sort(0);
+            Show(FormatWords(_vocabulary));
         }
 
         private void OnEnable()
@@ -40,37 +50,77 @@ namespace VocabularyModule.Data.View
 
         private void LoadVocabulary()
         {
-            _words = _vocabularyController.Vocabulary.Words;
+            _vocabulary = _vocabularyController.Vocabulary.Words;
             
-            if (_words == null)
+            if (_vocabulary == null)
                 Debug.LogError(gameObject.name+": words are null");
         }
 
         private void ShowSorted(int sortType)
         {
-            Sort(sortType);
-            Show();
+            searchInput.text = "";
+            if (_lastSortType != sortType)
+            {
+                Sort(sortType);
+                _lastSortType = sortType;
+            }
+
+            Show(FormatWords(_vocabulary));
+        }
+        
+        private void ShowSearched()
+        {
+            if (searchInput.text == string.Empty || searchInput.text == " " || searchInput.text == null)
+            {
+                Show(FormatWords(_vocabulary));
+                return;
+            }
+
+            var searchedWords = GetSearchedWords();
+            Show(FormatWords(searchedWords, true));
         }
 
         private void Sort(int sortType)
         {
-            _words = VocabularySortFactory.GetSorter(sortType).Sort(_words);
+            _vocabulary = VocabularySortFactory.GetSorter(sortType).Sort(_vocabulary);
         }
         
-        private void Show()
+        private List<Word> GetSearchedWords()
         {
-            var formattedVocabulary = new List<string> { new('-', 96) };
+            var search = searchInput.text.ToLower();
+            return _vocabulary
+                .Where(word => 
+                    word.Original.ToLower().Contains(search) 
+                    || 
+                    word.Translation.ToLower().Contains(search))
+                .ToList();
+        }
+        
+        private List<string> FormatWords(List<Word> words, bool isHighlightSearched = false)
+        {
+            var formattedWords = new List<string> { new('-', 96) };
 
-            foreach (var word in _words)
+            foreach (var word in words)
             {
                 var original = word.Original;
                 var translation = word.Translation;
                 
-                formattedVocabulary.Add($"{original} - {translation}");
-                formattedVocabulary.Add(new string('-', 96));
+                if (isHighlightSearched)
+                {
+                    original = original.Replace(searchInput.text, $"<u>{searchInput.text}</u>");
+                    translation = translation.Replace(searchInput.text, $"<u>{searchInput.text}</u>");
+                }
+                
+                formattedWords.Add($"{original} - {translation}");
+                formattedWords.Add(new string('-', 96));
             }
 
-            textField.text = String.Join("\n", formattedVocabulary);
+            return formattedWords;
+        }
+        
+        private void Show(List<string> wordsToShow)
+        {
+            textField.text = String.Join("\n", wordsToShow);
         }
     }
 }
